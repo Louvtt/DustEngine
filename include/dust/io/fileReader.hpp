c243505d51779b6dc6860d0b4f7862820650a552
Louvtt <louvttgames@gmail.com>
2023-08-11 22:00:02 +0200

    Added file reader for file utils functions or class.

---
+++ include/dust/io/fileReader.hpp
@@ -0,0 +1,17 @@
+#ifndef _DUST_IO_FILEREADER_HPP_
+#define _DUST_IO_FILEREADER_HPP_
+
+#include <string>
+
+namespace dust {
+
+namespace io {
+
+std::string getFileRawContent(const std::string& path);
+
+}
+
+}
+
+
+#endif //_DUST_IO_FILEREADER_HPP_
---
+++ src/io/fileReader.cpp
@@ -0,0 +1,30 @@
+#include "dust/io/fileReader.hpp"
+#include "dust/core/log.hpp"
+
+#include <fstream>
+#include <filesystem>
+#include <system_error>
+
+// from : https://insanecoding.blogspot.com/2011/11/how-to-read-in-file-in-c.html
+std::string dust::io::getFileRawContent(const std::string& path)
+{
+    std::error_code error{};
+    if(std::filesystem::exists(path, error)) {
+        DUST_ERROR("[File] {} doesn't exists (error {} : {})", path, error.value(), error.message());
+        return "";
+    }
+
+    std::ifstream in(path, std::ios::in);
+    in.exceptions(std::ios::badbit | std::ios::failbit);
+    if (in)
+    {
+        std::string contents;
+        in.seekg(0, std::ios::end);
+        contents.resize(in.tellg());
+        in.seekg(0, std::ios::beg);
+        in.read(&contents[0], contents.size());
+        in.close();
+        return(contents);
+    }
+    throw(errno);
+}
