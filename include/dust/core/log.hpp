d89662e1cfade632dec87f8fdfb9dc5e53737f1b
Louvtt <louvttgames@gmail.com>
2023-08-11 22:27:53 +0200

    Added debug log

--- include/dust/core/log.hpp
+++ include/dust/core/log.hpp
@@ -6,6 +6,7 @@
 #define DUST_ERROR(...) spdlog::error(__VA_ARGS__)
 #define DUST_INFO(...) spdlog::info(__VA_ARGS__)
 #define DUST_WARN(...) spdlog::warn(__VA_ARGS__)
+#define DUST_DEBUG(...) spdlog::debug(__VA_ARGS__)
 #define DUST_TRACE(...) spdlog::trace(__VA_ARGS__)
 
 #endif //_DUST_CORE_LOG_HPP_
