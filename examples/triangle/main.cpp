e5431e626baae95f00fcb44b4c213808a60d8035
Louvtt <louvttgames@gmail.com>
2023-08-13 04:22:38 +0200

    Added drawWireframe example in triangle example

--- examples/triangle/main.cpp
+++ examples/triangle/main.cpp
@@ -1,4 +1,6 @@
+#include "dust/core/log.hpp"
 #include "dust/dust.hpp"
+#include "dust/io/keycodes.hpp"
 #include "dust/render/mesh.hpp"
 #include "dust/render/shader.hpp"
 
@@ -29,9 +31,12 @@ private:
     dust::render::Shader m_shader;
     dust::render::Mesh m_triangle;
 
+    bool m_polygonLineMode{false};
+
 public:
     SimpleShaderApp() 
     : dust::Application("Triangle"),
+    m_polygonLineMode(false),
     m_shader(vCode, fCode),
     m_triangle(dust::render::MeshDescriptor{
         {   // pos               // color
@@ -47,6 +52,15 @@ public:
         }
     })
     {  }
+
+    void update() override
+    {
+        if(getInputManager()->isKey(dust::Key::P, dust::KeyState::Press))
+        {
+            m_polygonLineMode = !m_polygonLineMode;
+            getRenderer()->setDrawWireframe(m_polygonLineMode);
+        }
+    }
 
     void render() override 
     {
