628647dd416accf202e1e1e136392901b423a9ee
Louvtt <louvttgames@gmail.com>
2023-08-14 01:32:34 +0200

    Added depth shader in sponza example

---
+++ examples/sponza/assets/depth.frag
@@ -0,0 +1,21 @@
+#version 460 core
+out vec4 FragColor;
+
+in vec4 oColor;
+
+float near = 0.1; 
+float far  = 1000.0; 
+  
+float LinearizeDepth(float depth) 
+{
+    float z = depth * 2.0 - 1.0; // back to NDC 
+    return (2.0 * near * far) / (far + near - z * (far - near));	
+}
+
+void main()
+{           
+    if(oColor.a < .5) discard;
+      
+    float depth = LinearizeDepth(gl_FragCoord.z) / far; // divide by far for demonstration
+    FragColor = vec4(vec3(depth), 1.0);
+}
--- examples/sponza/main.cpp
+++ examples/sponza/main.cpp
@@ -3,6 +3,7 @@
 #include "dust/io/inputManager.hpp"
 #include "dust/io/keycodes.hpp"
 #include "dust/render/camera.hpp"
+#include "dust/render/shader.hpp"
 
 using namespace dust;
 
@@ -11,6 +12,9 @@ class SimpleApp
 {
 private:
     render::Shader *m_shader;
+    render::Shader *m_depthShader;
+    render::Shader *m_currentShader;
+
     render::Model *m_sponza;
     render::Camera3D *m_camera;
 
@@ -26,14 +30,20 @@ public:
             DUST_ERROR("Exiting ... Shader missing");
             exit(-1);
         }
-
+        m_depthShader = render::Shader::loadFromFile("assets/shader.vert", "assets/depth.frag");
+        if(!(bool)m_depthShader) {
+            DUST_ERROR("Exiting ... Shader missing");
+            exit(-1);
+        }
+        m_currentShader = m_depthShader;
+        
         m_sponza = render::Model::loadFromFile("assets/sponza/sponza.obj");
         if(!m_sponza) {
             DUST_ERROR("Exiting ... Sponza missing");
             exit(-1);
         }
 
-        m_camera->bind(m_shader);
+        m_camera->bind(m_currentShader);
     }
 
     void update() override
@@ -41,23 +51,35 @@ public:
         f32 delta = (f32)getTime().delta;
         if(InputManager::IsKeyDown(Key::Q)) {
             m_camera->rotate({-10.f * delta, .0f, .0f});
+            m_camera->bind(m_currentShader);
         }
         if(InputManager::IsKeyDown(Key::D)) {
             m_camera->rotate({10.f * delta, .0f, .0f});
+            m_camera->bind(m_currentShader);
         }
 
         if(InputManager::IsKeyDown(Key::W)) {
             m_camera->move(m_camera->forward() * 100.f * delta);
+            m_camera->bind(m_currentShader);
         }
         if(InputManager::IsKeyDown(Key::S)) {
             m_camera->move(m_camera->forward() * -100.f * delta);
+            m_camera->bind(m_currentShader);
+        }
+
+        // reload shaders ?
+        if(InputManager::IsKeyPressed(Key::R)) {
+            m_shader->reload();
+            m_depthShader->reload();
+            m_camera->bind(m_shader);
+            m_camera->bind(m_depthShader);
         }
     }
 
     void render() override
     {
         m_camera->resize(getWindow()->getWidth(), getWindow()->getHeight());
-        m_sponza->draw(m_shader);
+        m_sponza->draw(m_currentShader);
     }
 };
 
