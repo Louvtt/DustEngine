53e4b0839ce48f8dada69e3f64f518217e9977a9
Louvtt <louvttgames@gmail.com>
2023-08-14 05:03:58 +0200

    Updated Model to load Materials

--- src/render/model.cpp
+++ src/render/model.cpp
@@ -2,11 +2,16 @@
 
 #include "assimp/Importer.hpp"
 #include "assimp/Vertex.h"
+#include "assimp/color4.h"
+#include "assimp/material.h"
 #include "assimp/mesh.h"
 #include "assimp/postprocess.h"
 #include "assimp/scene.h"
+#include "assimp/types.h"
 #include "dust/core/log.hpp"
 #include "dust/render/mesh.hpp"
+#include "dust/render/material.hpp"
+#include "dust/render/texture.hpp"
 #include <filesystem>
 
 namespace dr = dust::render;
@@ -17,10 +22,48 @@ m_modelMat(1.f)
 { }
 dr::Model::~Model()
 {
+    for(auto mesh : m_meshes) {
+        delete mesh;
+    }
     m_meshes.clear();
 }
 
-static dr::Mesh* processMesh(aiMesh *mesh)
+static std::vector<dr::Material*> 
+processMaterials(const aiScene *scene, const std::filesystem::path& basePath)
+{
+    std::vector<dr::Material*> materials{};
+    materials.reserve(scene->mNumMaterials);
+    for(int i = 0; i < scene->mNumMaterials; ++i) {
+        auto material = scene->mMaterials[i];
+        DUST_DEBUG("[Model] Material {}", i);
+        aiString filePath;
+        dr::Texture* diffuseTexture = nullptr;
+        if(material->GetTexture(aiTextureType_DIFFUSE, 0, &filePath) == AI_SUCCESS) {
+            const std::string texPath = basePath.string() + '/' + filePath.C_Str();
+            DUST_DEBUG("Texture found for mat at {}", texPath);
+            diffuseTexture = new dust::render::Texture(texPath);
+        }
+
+        glm::vec4 diffuse(1.f);
+        aiColor4D diffuseMat;
+        if(aiGetMaterialColor(material, AI_MATKEY_COLOR_DIFFUSE, &diffuseMat) == AI_SUCCESS) {
+            diffuse = {diffuseMat.r, diffuseMat.g, diffuseMat.b, diffuseMat.a};
+        }
+
+        glm::vec4 ambient(1.f);
+        aiColor4D ambientMat;
+        if(aiGetMaterialColor(material, AI_MATKEY_COLOR_AMBIENT, &ambientMat) == AI_SUCCESS) {
+            ambient = {ambientMat.r, ambientMat.g, ambientMat.b, ambientMat.a};
+        }
+
+        materials.push_back(new dust::render::PBRMaterial(
+            diffuseTexture, ambient, diffuse
+        ));
+    }
+    return materials;
+}
+
+static dr::Mesh* processMesh(aiMesh *mesh, const std::vector<dr::Material*> &materials)
 {
     std::vector<dr::Model::Vertex> vertices;
     vertices.reserve(mesh->mNumVertices);
@@ -67,7 +110,9 @@ static dr::Mesh* processMesh(aiMesh *mesh)
         indices.push_back(face.mIndices[2]);
     }
 
-    return new dr::Mesh(&vertices.front(), sizeof(dr::Model::Vertex), mesh->mNumVertices, indices, attributes);
+    auto res = new dr::Mesh(&vertices.front(), sizeof(dr::Model::Vertex), mesh->mNumVertices, indices, attributes);
+    res->setMaterial(materials.at(mesh->mMaterialIndex));
+    return res;
 }
 
 dr::Model* dr::Model::loadFromFile(const std::string& path)
@@ -82,19 +127,24 @@ dr::Model* dr::Model::loadFromFile(const std::string& path)
         aiProcess_Triangulate
     );
 
+    std::filesystem::path modelPathDir = std::filesystem::path(path).parent_path();
+    auto materials = processMaterials(scene, modelPathDir);
+
     std::vector<dr::Mesh*> processedMeshes{};
     for (int i = 0; i < scene->mNumMeshes; ++i) {
-        processedMeshes.push_back(processMesh(scene->mMeshes[i]));
+        processedMeshes.push_back(processMesh(scene->mMeshes[i], materials));
     }
+
+    importer.FreeScene();
+
     return new Model(processedMeshes);
 }
 
 
 void dr::Model::draw(Shader *shader)
 {
-    shader->use();
     shader->setUniform("uModel", m_modelMat);
     for (auto mesh : m_meshes) {
-        mesh->draw();
+        mesh->draw(shader);
     }
 }
