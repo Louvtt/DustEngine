65b2b2ba168bdc996b07ac6da06ced9d47eeb5d8
Louvtt <53311833+Louvtt@users.noreply.github.com>
2024-11-11 22:06:16 +0100

    Added includes in main headerlib file

--- include/dust/dust.hpp
+++ include/dust/dust.hpp
@@ -3,6 +3,8 @@
 // ---------------------------------
 #include "core/application.hpp"
 #include "core/types.hpp"
+#include "core/log.hpp"
+#include "core/profiling.hpp"
 
 // ---------------------------------
 // Render includes
@@ -11,3 +13,20 @@
 #include "render/shader.hpp"
 #include "render/mesh.hpp"
 #include "render/model.hpp"
+#include "render/camera.hpp"
+#include "render/framebuffer.hpp"
+#include "render/light.hpp"
+#include "render/material.hpp"
+#include "render/renderPass.hpp"
+#include "render/texture.hpp"
+#include "render/skybox.hpp"
+
+// ---------------------------------
+// IO includes
+// ---------------------------------
+#include "io/inputManager.hpp"
+#include "io/keycodes.hpp"
+#include "io/loaders.hpp"
+#include "io/fileReader.hpp"
+#include "io/resourceManager.hpp"
+
